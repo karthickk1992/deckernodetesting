05627e771d6a50e12322f03b2b04e2f55edd88ec
karthick <karthick.infatech@gmail.com>
2023-07-23 11:25:50 +0530

    docker file from server

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const http = require('http');
 
 const hostname = '127.0.0.1';
-const port = 1000;
+const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.end('Hello World node docker');
 });
  
-server.listen(port, hostname, () => {
+server.listen(port,  () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
